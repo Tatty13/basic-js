d8b786dbd2f3d2b4d056f47c9465350cefb67455
Tatty13 <tat.mi.iva@gmail.com>
2022-09-03 18:00:45 +0300

    feat: add a solution for the vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = [];
+    for (let i = 65; i < 91; i++) {
+      this.alphabet.push(String.fromCharCode(i))
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getMessageInKey (message, key) {
+    let messageOfKey = '';
+    let index = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        messageOfKey += key[index % key.length];
+        index++;
+      } else {
+        messageOfKey += message[i];
+      }
+    }
+    return messageOfKey;
+  }
+  
+  encrypt(message, key) {
+    if (arguments.length !== 2 || typeof message !== 'string' || typeof key !== 'string') throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let messageInKey = this.getMessageInKey(message, key);
+    let result = '';
+
+    for (let i = 0; i < messageInKey.length; i++) {
+      if (this.alphabet.includes(messageInKey[i])) {
+        let letterIndex = (this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(messageInKey[i])) % this.alphabet.length;
+        result += this.alphabet[letterIndex];
+      } else {
+        result += messageInKey[i]
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (arguments.length !== 2 || typeof encryptedMessage !== 'string' || typeof key !== 'string') throw new Error('Incorrect arguments!');
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let encryptedMessageInKey = this.getMessageInKey(encryptedMessage, key);
+    let result = '';
+
+    for (let i = 0; i < encryptedMessageInKey.length; i++) {
+      if (this.alphabet.includes(encryptedMessageInKey[i])) {
+        let letterIndex = (this.alphabet.indexOf(encryptedMessage[i]) - this.alphabet.indexOf(encryptedMessageInKey[i])) % this.alphabet.length;
+        if (letterIndex < 0) letterIndex = this.alphabet.length + letterIndex;
+        result += this.alphabet[letterIndex];
+      } else {
+        result += encryptedMessageInKey[i];
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
 
