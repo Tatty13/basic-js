e7abb544acbdbd71345a6fe186ee9599f7667a89
Tatty13 <tat.mi.iva@gmail.com>
2022-09-03 20:07:30 +0300

    feat: add a solution for the delete-digit task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit2( n ) {
+  let arrOfNums = n.toString(10).split('');
+
+  for (let i = 0; i < arrOfNums.length - 1; i++) {
+    if (arrOfNums[i] === arrOfNums[i + 1]) continue;
+    if (arrOfNums[i] > arrOfNums[i + 1]) {
+      arrOfNums.splice(i + 1, 1);
+      return +arrOfNums.join('');
+    } else {
+      arrOfNums.splice(i, 1);
+      return +arrOfNums.join('');
+    }
+  }  
+}
+
+function deleteDigit(n) {
+  let arrOfNums = n.toString(10).split('');
+  let min = `${Math.min(...arrOfNums)}`;
+  let arrOfNumsWithoutMinInt = [...arrOfNums];
+  
+  arrOfNumsWithoutMinInt.splice(arrOfNums.indexOf(min), 1);
+  arrOfNums.splice(0, 1);
+  
+  return +arrOfNumsWithoutMinInt.join('') > +arrOfNums.join('') ? 
++arrOfNumsWithoutMinInt.join('') : +arrOfNums.join('')
 }
 
 module.exports = {
